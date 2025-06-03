eb5ebd278bcb2d6e91285ba4c700641831c443ed
Peter Torki <pjturki@gmail.com>
2025-06-03 21:51:17 +0300

    cron added and done

--- src/server.js
+++ src/server.js
@@ -4,14 +4,21 @@ import { initDB } from "./config/db.js"; // Adjust the path as necessary
 import rateLimiter from "./middleware/rateLimiter.js"; // Adjust the path as necessary
 
 import transactionsRoute from "./routes/transactionsRoute.js"; // Adjust the path as necessary
+import job from "./config/cron.js";
 
 const app = express();
+
+if (process.env.NODE_ENV === "production") job.start();
 
 // middleware to parse JSON bodies
 app.use(rateLimiter);
 app.use(express.json()); // Middleware to parse JSON bodies
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5002;
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ status: "OK" });
+});
 
 // our custom simple middleware
 // app.use((req, res, next) => {
